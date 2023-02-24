0eca0b15f69c7d65c054d9129c29f0b4ca0c2e39
vadychka <44154644+vadychka@users.noreply.github.com>
2023-02-24 07:56:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,11 +9,7 @@ require('dotenv').config()
 mongoose.set("strictQuery", false);
 
 mongoose.connect(process.env.MONGODB_URL, retryWrites=true, {useNewUrlParser: true})
-mongoose.connection.once('open', function(){
-  console.log('Conection has been made!');
-      }).on('error', function(error){
-   console.log('Error is: ', error);
-    });
+console.log(111)
 
 const bot = new TelegramApi(process.env.TOKEN, {polling:true})
 
